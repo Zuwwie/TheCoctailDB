d046aa17b5e37f9898c44cf505f68b9c5362e870
Nazik <nazar.gavronsky.mail@gmail.com>
2020-05-05 17:47:19 +0300

    rout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './coponents/header/header.component';
 import { FooterComponent } from './coponents/footer/footer.component';
+import { AppRoutingModule } from './app-routing.modules';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [AppRoutingModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
