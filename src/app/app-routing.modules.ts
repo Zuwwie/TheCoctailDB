42344fc2681607fc293ba820c2310fb80294bffb
Nazik <nazar.gavronsky.mail@gmail.com>
2020-05-05 22:24:56 +0300

    pages

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
+import { MissingComponent } from './pages/missing/missing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'missing', component: MissingComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'contact', component: ContactComponent },
+  //   { path: '', component:  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,25 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 import { MainComponent } from './pages/main/main.component';
 import { InfoComponent } from './pages/info/info.component';
 import { BrowseComponent } from './pages/browse/browse.component';
+import { MissingComponent } from './pages/missing/missing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, PageNotFoundComponent, MainComponent, InfoComponent, BrowseComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    MainComponent,
+    InfoComponent,
+    BrowseComponent,
+    MissingComponent,
+    AboutComponent,
+    FaqComponent,
+    ContactComponent,
+  ],
   imports: [AppRoutingModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
