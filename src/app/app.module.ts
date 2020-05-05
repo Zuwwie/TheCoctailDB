978ad7620e9d7c64b2cfeadc7cec8e496bc6c63a
Nazik <nazar.gavronsky.mail@gmail.com>
2020-05-05 17:59:13 +0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './coponents/header/header.component';
 import { FooterComponent } from './coponents/footer/footer.component';
 import { AppRoutingModule } from './app-routing.modules';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
+import { InfoComponent } from './pages/info/info.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PageNotFoundComponent, MainComponent, InfoComponent],
   imports: [AppRoutingModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
