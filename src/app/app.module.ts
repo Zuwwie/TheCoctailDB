e0a338b5d7fab5343a8216714d7eb04ebbd22d3b
Nazik <nazar.gavronsky.mail@gmail.com>
2020-05-05 16:07:11 +0300

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './coponents/header/header.component';
+import { FooterComponent } from './coponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
